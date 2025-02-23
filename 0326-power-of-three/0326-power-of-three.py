1051fb6d77393591da6198a3e713326e4b214e0d
Ananye Shobhith Gajula <gajulaa1@montclair.edu>
2025-02-22 20:14:39 -0500

    Time: 241 ms (5.05%), Space: 17.7 MB (73.54%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -3,7 +3,9 @@ class Solution:
         for i in range(0,100):
             if(n == 3**i):
                 return True
+            elif(n<=0):
+                return False
             else:
                 continue
-            return False
+        return False
         
