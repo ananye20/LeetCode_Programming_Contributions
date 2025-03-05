0e0b07fb21bca13c6691a615e31534afe77cf470
Ananye Shobhith Gajula <gajulaa1@montclair.edu>
2025-03-04 23:57:45 -0500

    Time: 0 ms (100%), Space: 18.6 MB (26.83%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(0, len(nums)):
+            if(target<=nums[i]):
+                return i
+                break
+        return len(nums)
+        
+
+        
