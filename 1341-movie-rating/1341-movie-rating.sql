4f10f4ca57236b4acad88eb3c0338895cf2bf85b
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-03-13 17:34:17 -0400

    Time: 1183 ms (97.44%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,21 +1,22 @@
+WITH userct as (SELECT name as results
+FROM Users U
+RIGHT JOIN MovieRating MR
+ON U.user_id = MR.user_id
+GROUP BY name
+HAVING COUNT(MR.user_id)>0
+ORDER BY COUNT(MR.user_id) DESC, name ASC
+LIMIT 1),
 
-SELECT results FROM (
-    SELECT U.name AS results
-    FROM Users U
-    JOIN MovieRating MR ON U.user_id = MR.user_id
-    GROUP BY U.user_id, U.name
-    ORDER BY COUNT(MR.movie_id) DESC, U.name ASC
-    LIMIT 1
-) AS user_ranking
+mvename as (SELECT title as results
+FROM MovieRating MR
+LEFT JOIN Movies M
+ON MR.movie_id = M.movie_id
+WHERE YEAR(created_at)='2020' AND MONTH(created_at)='02'
+GROUP BY title
+HAVING AVG(rating)>0
+ORDER BY AVG(rating) DESC, title ASC
+LIMIT 1)
 
+SELECT * FROM userct
 UNION ALL
-
-SELECT results FROM (
-    SELECT M.title AS results
-    FROM Movies M
-    JOIN MovieRating MR ON M.movie_id = MR.movie_id
-    WHERE YEAR(MR.created_at) = 2020 AND MONTH(MR.created_at) = 2
-    GROUP BY M.movie_id, M.title
-    ORDER BY AVG(MR.rating) DESC, M.title ASC
-    LIMIT 1
-) AS movie_ranking;
+SELECT * FROM mvename
