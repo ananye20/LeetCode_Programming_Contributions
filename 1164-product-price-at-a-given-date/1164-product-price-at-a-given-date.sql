5441d8111d71120983787b3fb1dd5e5ea33d208a
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-03-16 18:35:01 -0400

    Time: 892 ms (57.92%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,9 @@
+
+Select distinct product_id, new_price as price
+From Products
+Where (product_id, change_date) in (Select product_id, max(change_date) From Products where change_date<='2019-08-16' group by product_id)
+union all
+Select product_id, 10 as price
+from products 
+group by product_id
+having min(change_date)>'2019-08-16'
