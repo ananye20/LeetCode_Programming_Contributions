84a464a87503f0a6ba6b8b06b029af44f5d0af60
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-03-16 21:11:41 -0400

    Time: 419 ms (84.98%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,19 +1,13 @@
-SELECT id, COUNT(*) AS num
-FROM (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id AS id FROM RequestAccepted
-) combined
+WITH cte as(SELECT requester_id as id
+FROM RequestAccepted
+
+UNION ALL
+
+SELECT accepter_id as id
+FROM RequestAccepted)
+
+SELECT id, COUNT(id) as num
+FROM cte
 GROUP BY id
-HAVING num = (
-    SELECT MAX(num_count) 
-    FROM (
-        SELECT id, COUNT(*) AS num_count
-        FROM (
-            SELECT requester_id AS id FROM RequestAccepted
-            UNION ALL
-            SELECT accepter_id AS id FROM RequestAccepted
-        ) inner_combined
-        GROUP BY id
-    ) max_table
-);
+ORDER BY num DESC
+LIMIT 1
