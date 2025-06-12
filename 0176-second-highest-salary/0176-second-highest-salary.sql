329a451a1ea748e1233f554b605a94f0051137e7
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-06-12 18:49:37 -0400

    Time: 265 ms (81.4%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,3 @@
-SELECT IFNULL(
-    (SELECT DISTINCT salary
-     FROM Employee
-     ORDER BY salary DESC
-     LIMIT 1 OFFSET 1),
-    NULL
-) AS SecondHighestSalary;
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
